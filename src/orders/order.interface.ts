ab367f758f0f7f0de81f75c2377f108fe7f6cbd7
BekmurodFoziloff <bekmurodfoziloff@gmail.com>
2023-01-02 19:43:55 +0500

    feat(Orders): create interface of Orders

---
+++ src/orders/order.interface.ts
@@ -0,0 +1,58 @@
+import { Document } from 'mongoose';
+import { Specialization } from '../specializations/specialization.interface';
+import { User } from '../users/user.interface';
+
+interface GeneralInformationAboutTheCustomer extends Document {
+    id: string;
+    customer: string;
+    customerType: string;
+    applicationsOpen: Date;
+    applicationsClose: Date;
+    minBudget: number;
+    maxBudget: number;
+    currency: string;
+    negotiable: string;
+    filingConditions: string;
+    specializations: Specialization[];
+}
+
+interface GeneralInformationAboutTheProject extends Document {
+    id: string;
+    title: string;
+    description: string;
+    deadline: Date;
+    comments: string;
+    attachedFile: string;
+}
+
+interface GeneralRequirementsToTheExecutor extends Document {
+    id: string;
+    expectedTransactionType: string;
+    requirementsToTheExecutor: string;
+    commentsToTheRequirements: string;
+}
+
+interface ContactInformation extends Document {
+    id: string;
+    customerWebsite: string;
+    customerEmail: string;
+    customerAddress: string;
+    customerPhoneNumber: string;
+    customerAddPhoneNumber: string;
+    personEmail: string;
+    personAddress: string;
+    personPhoneNumber: string;
+    personAddPhoneNumber: string;
+}
+
+export interface Order extends Document {
+    id: string;
+    customer: GeneralInformationAboutTheCustomer;
+    project: GeneralInformationAboutTheProject;
+    requirement: GeneralRequirementsToTheExecutor;
+    contact: ContactInformation;
+    owner: User;
+    condition: string;
+    created?: string;
+    updated?: string;
+}
